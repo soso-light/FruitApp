ccb7893aa9e6b2337974772cbe6d98f334e17404
soso-light <80017031+soso-light@users.noreply.github.com>
2021-09-12 22:25:57 +0900

    made list

---
+++ FruitApp/src/Fruit.java
@@ -0,0 +1,14 @@
+
+
+public class Fruit {
+	private int no;
+	private String name;
+	private int price;
+	
+	public Fruit (int no, String name, int price) {
+		super();
+		this.no = no;
+		this.name = name;
+		this.price = price;
+	}
+}
---
+++ FruitApp/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Fruit> fl = new ArrayList<Fruit>();
+		fl.add(new Fruit(101, "apple", 1500));
+		fl.add(new Fruit(102, "banana", 1000));
+		fl.add(new Fruit(103, "grape", 2500));
+		fl.add(new Fruit(104, "mandarin", 500));
+		fl.add(new Fruit(105, "pineapple", 3500));
+		fl.add(new Fruit(106, "peach", 1500));
+		fl.add(new Fruit(107, "strawberry", 2500));
+		fl.add(new Fruit(108, "green apple", 1000));
+		fl.add(new Fruit(109, "pear", 1520));
+		fl.add(new Fruit(110, "tomato", 1300));
+		
+	}
+
+}
